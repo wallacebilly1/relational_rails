0c7cf749ddc08184f6aaddf6b8f6fd8fe3754c0e
Billy Wallace <wallace.billy1@gmail.com>
2024-03-21 15:08:31 -0600

    Add tests for User Story 8 and write code to pass test

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -1,6 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Players Index Page", type: :feature do
+  before :all do
+    @nuggets = Team.create!(name: "Nuggets", city: "Denver", single_city: true, total_cap: 185751085)
+    @lakers = Team.create!(name: "Lakers", city: "Los Angeles", single_city: false, total_cap: 182033999)
+    @thunder = Team.create!(name: "Thunder", city: "Oklahoma City", single_city: true, total_cap: 160502705)
+
+    @jokic = @nuggets.players.create!(name:"Nikola Jokic", age: 29, international: true)
+    @murray = @nuggets.players.create!(name:"Jamal Murray", age: 27, international: true)
+    @lebron = @lakers.players.create!(name:"Lebron James", age: 39, international: false)
+    @davis = @lakers.players.create!(name:"Anthony Davis", age: 31, international: false)
+    @shai = @thunder.players.create!(name:"Shai Gilgeous-Alexander", age: 25, international: true)
+    @chet = @thunder.players.create!(name:"Chet Holmgren", age: 21, international: false)
+  end
+  
   describe "User Story 3" do
     # User Story 3, Child Index 
 
@@ -11,17 +24,6 @@ RSpec.describe "Players Index Page", type: :feature do
     describe "As a visitor" do
       describe "When I visit '/players'" do
         it "Then I see each player in the system including the player's attributes" do
-          #arrange
-          nuggets = Team.create!(name: "Nuggets", city: "Denver", single_city: true, total_cap: 185751085)
-          lakers = Team.create!(name: "Lakers", city: "Los Angeles", single_city: false, total_cap: 182033999)
-          thunder = Team.create!(name: "Thunder", city: "Oklahoma City", single_city: true, total_cap: 160502705)
-
-          jokic = Player.create!(name:"Nikola Jokic", age: 29, international: true, team_id: nuggets.id)
-          murray = Player.create!(name:"Jamal Murray", age: 27, international: true, team_id: nuggets.id)
-          lebron = Player.create!(name:"Lebron James", age: 39, international: false, team_id: lakers.id)
-          davis = Player.create!(name:"Anthony Davis", age: 31, international: false, team_id: lakers.id)
-          shai = Player.create!(name:"Shai Gilgeous-Alexander", age: 25, international: true, team_id: thunder.id)
-          chet = Player.create!(name:"Chet Holmgren", age: 21, international: false, team_id: thunder.id)
           #act
           visit "/players"
           #assert
@@ -29,6 +31,28 @@ RSpec.describe "Players Index Page", type: :feature do
           expect(page).to have_content('Age: 29')
           expect(page).to have_content('International Player: true')
           expect(page).to have_content('Team: Nuggets')
+        end
+      end
+    end
+  end
+
+  describe "User Story 8" do
+    # User Story 8, Child Index Link
+
+    # As a visitor
+    # When I visit any page on the site
+    # Then I see a link at the top of the page that takes me to the Child Index
+    describe "As a visitor" do
+      describe "When I visit any page on the site" do
+        it "Then I see a link at the top of the page that takes me to the Players Index" do
+          #act
+          visit "/teams"
+          #assert
+          expect(page).to have_link('Players', href: '/players')
+          #act
+          visit "/teams/#{@nuggets.id}"
+          #assert
+          expect(page).to have_link('Players', href: '/players')
         end
       end
     end
