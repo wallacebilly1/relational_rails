e1ea0af45ab2633152e07b4187b09f3f3fab19cb
Billy Wallace <wallace.billy1@gmail.com>
2024-03-21 15:12:36 -0600

    Add tests for User Story 9 and write code to pass test

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe "Teams Index Page", type: :feature do
     @nuggets = Team.create!(name: "Nuggets", city: "Denver", single_city: true, total_cap: 185751085)
     @lakers = Team.create!(name: "Lakers", city: "Los Angeles", single_city: false, total_cap: 182033999)
     @thunder = Team.create!(name: "Thunder", city: "Oklahoma City", single_city: true, total_cap: 160502705)
+
+    @jokic = @nuggets.players.create!(name:"Nikola Jokic", age: 29, international: true)
+    @murray = @nuggets.players.create!(name:"Jamal Murray", age: 27, international: true)
+    @lebron = @lakers.players.create!(name:"Lebron James", age: 39, international: false)
+    @davis = @lakers.players.create!(name:"Anthony Davis", age: 31, international: false)
+    @shai = @thunder.players.create!(name:"Shai Gilgeous-Alexander", age: 25, international: true)
+    @chet = @thunder.players.create!(name:"Chet Holmgren", age: 21, international: false)
   end
+
   describe "User Story 1" do
     # User Story 1, Parent Index 
 
@@ -48,6 +56,28 @@ RSpec.describe "Teams Index Page", type: :feature do
           visit "/teams"
           #assert
           expect(page).to have_content("Name: #{@nuggets.name}, Created At: #{@nuggets.created_at}")
+        end
+      end
+    end
+  end
+
+  describe "User Story 9" do
+    # User Story 9, Parent Index Link
+
+    # As a visitor
+    # When I visit any page on the site
+    # Then I see a link at the top of the page that takes me to the Parent Index
+    describe "As a visitor" do
+      describe "When I visit any page on the site" do
+        it "Then I see a link at the top of the page that takes me to the Team Index" do
+          #act
+          visit "/players"
+          #assert
+          expect(page).to have_link('Teams', href: '/teams')
+          #act
+          visit "/teams/#{@nuggets.id}"
+          #assert
+          expect(page).to have_link('Teams', href: '/teams')
         end
       end
     end
