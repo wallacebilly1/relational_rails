7a913e0d0303141a68110c07f6e98ffe47aaa45c
Billy Wallace <wallace.billy1@gmail.com>
2024-03-21 15:56:41 -0600

    Add tests for User Story 10 and write code to pass test

--- spec/features/teams/players/index_spec.rb
+++ spec/features/teams/players/index_spec.rb
@@ -1,6 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Teams Players Index Page", type: :feature do
+  before :all do
+    @nuggets = Team.create!(name: "Nuggets", city: "Denver", single_city: true, total_cap: 185751085)
+    @lakers = Team.create!(name: "Lakers", city: "Los Angeles", single_city: false, total_cap: 182033999)
+    @thunder = Team.create!(name: "Thunder", city: "Oklahoma City", single_city: true, total_cap: 160502705)
+
+    @jokic = @nuggets.players.create!(name:"Nikola Jokic", age: 29, international: true)
+    @murray = @nuggets.players.create!(name:"Jamal Murray", age: 27, international: true)
+    @lebron = @lakers.players.create!(name:"Lebron James", age: 39, international: false)
+    @davis = @lakers.players.create!(name:"Anthony Davis", age: 31, international: false)
+    @shai = @thunder.players.create!(name:"Shai Gilgeous-Alexander", age: 25, international: true)
+    @chet = @thunder.players.create!(name:"Chet Holmgren", age: 21, international: false)
+  end
+  
   describe "User Story 5" do
     # User Story 5, Parent Children Index 
 
@@ -11,12 +24,8 @@ RSpec.describe "Teams Players Index Page", type: :feature do
     describe "As a visitor" do
       describe "When I visit '/teams/:team_id/players" do
         it "Then I see each player that is associated with the team and their attributes" do
-          #arrange
-          nuggets = Team.create!(name: "Nuggets", city: "Denver", single_city: true, total_cap: 185751085)
-          jokic = Player.create!(name:"Nikola Jokic", age: 29, international: true, team_id: nuggets.id)
-          murray = Player.create!(name:"Jamal Murray", age: 27, international: true, team_id: nuggets.id)
           #act
-          visit "/teams/#{nuggets.id}/players"
+          visit "/teams/#{@nuggets.id}/players"
           #assert
           expect(page).to have_content('Denver Nuggets Players')
           expect(page).to have_content('Name: Nikola Jokic')
@@ -25,6 +34,28 @@ RSpec.describe "Teams Players Index Page", type: :feature do
           expect(page).to have_content('Name: Jamal Murray')
           expect(page).to have_content('Age: 27')
           expect(page).to have_content('International Player: true')
+        end
+      end
+    end
+  end
+
+  describe "User Story 10" do
+    # User Story 10, Parent Child Index Link
+
+    # As a visitor
+    # When I visit a Team show page ('/teams/:id')
+    # Then I see a link that takes me to that team's players page ('teams/:id/players)
+    describe "As a visitor" do
+      describe "When I visit a team's show page" do
+        it "Then I see a link that takes me to the team's players page" do
+          #act
+          visit "/teams/#{@nuggets.id}"
+          #assert
+          expect(page).to have_link("Roster", href: "/teams/#{@nuggets.id}/players")
+          #act
+          visit "/teams/#{@lakers.id}"
+          #assert
+          expect(page).to have_link("Roster", href: "/teams/#{@lakers.id}/players")
         end
       end
     end
